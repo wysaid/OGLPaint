176d879f29a6221aa6592c788f0baa27d0b327e2
wysaid <admin@wysaid.org>
2013-12-07 21:48:30 +0800

    recommit

---
+++ src/OGLPaint.cpp
@@ -0,0 +1,526 @@
+#include "stdafx.h"
+#include "resource.h"
+#include "OGLPaint.h"
+#define MAX_LOADSTRING 100
+
+HINSTANCE hInst;					
+
+HWND		g_hwnd;
+HDC			g_hdc;
+HGLRC		g_hRC=NULL;	
+HINSTANCE	g_hInstance;
+
+RECT		g_rect;
+RECT		g_clientRect;
+
+int			g_scrWidth = 800;
+int			g_scrHeight = 600;
+int			g_cliWidth;
+int			g_cliHeight;
+
+GLfloat		g_aspect;
+vector<PicElem*> vec;
+MODE		g_mode = modeLINES;
+
+float		g_defSize = 1.0f;
+// float		g_defWidth = 1.0f;
+Color		g_defColor(1.0f,1.0f,1.0f);
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
+{
+	
+	MSG msg;
+	HACCEL hAccelTable;
+	
+	vec.push_back(new Points);
+	vec.push_back(new Lines);
+//	vec.push_back(new Lines);
+
+	MyRegisterClass(hInstance);
+	if (!InitInstance (hInstance, nCmdShow)) 
+	{
+		return FALSE;
+	}
+	GetClientRect(g_hwnd,&g_clientRect);
+	g_cliWidth = g_clientRect.right - g_clientRect.left;
+	g_cliHeight = g_clientRect.bottom - g_clientRect.top;
+	EnableOpenGL();
+	SceneInit(g_clientRect.right, g_clientRect.bottom);
+	GetWindowRect(g_hwnd, &g_rect);
+	g_scrWidth = g_rect.right - g_rect.left;
+	g_scrHeight = g_rect.bottom - g_rect.top;
+	if(g_cliWidth > 0 && g_cliHeight > 0)
+		SceneResizeViewport(g_cliWidth, g_cliHeight);
+	hAccelTable = LoadAccelerators(hInstance, (LPCTSTR)IDC_XY);
+	SceneShow();
+	while (GetMessage(&msg, NULL, 0, 0)) 
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) 
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	DisableOpenGL();
+	DestroyWindow(g_hwnd);
+	for(size_t i = 0; i != vec.size(); ++i)
+		delete vec[i];
+	vec.clear();
+	ChangeDisplaySettings(NULL,0);
+	
+	return msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+	
+	wcex.cbSize = sizeof(WNDCLASSEX); 
+	
+	wcex.style			= CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= (WNDPROC)WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, (LPCTSTR)IDI_XY);
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wcex.lpszMenuName	= (LPCSTR)IDC_XY;
+	wcex.lpszClassName	= "Name";
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, (LPCTSTR)IDI_SMALL);
+	
+	return RegisterClassEx(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; 
+	
+	g_hwnd = CreateWindow("Name", "wysaid", WS_TILEDWINDOW | WS_VISIBLE,
+		CW_USEDEFAULT, 0, g_scrWidth, g_scrHeight, NULL, NULL, hInstance, NULL);
+	
+	if (!g_hwnd)
+	{
+		return FALSE;
+	}
+
+	FileInitialize (g_hwnd);
+	ChangeDisplaySettings(NULL, 0);
+	ShowWindow(g_hwnd, nCmdShow);
+	UpdateWindow(g_hwnd);	
+	return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	static isKeydown = false;
+	int wmId, wmEvent;
+/*	int tempX,tempY;*/
+	switch (message) 
+	{
+	case WM_COMMAND:
+	{
+		wmId    = LOWORD(wParam); 
+		wmEvent = HIWORD(wParam); 
+		
+		switch (wmId)
+		{
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		case IDM_PENCIL:
+			g_mode = modePENCIL;
+			break;
+		case IDM_POINTS:
+			g_mode = modePOINTS;
+			break;
+		case IDM_LINES:
+			g_mode = modeLINES;
+			break;
+		case IDM_Polygon:
+			g_mode = modePOLYGON;
+			break;
+		case IDM_CIRCLE:
+			g_mode = modeCIRCLE;
+			break;
+		case IDM_COLOR:
+			DialogBox(hInst, (LPCTSTR)IDD_COLORBOX, hWnd, (DLGPROC)colorBox);
+			break;
+		case IDM_SIZEBOX:
+			DialogBox(hInst, (LPCTSTR)IDD_DEFAULTWIDTH, hWnd, (DLGPROC)sizeBox);
+			break;
+		case IDM_CLEAR:
+			{
+				vec[0]->clear();
+				vec[1]->clear();
+				for(size_t i=2; i < vec.size(); ++i)
+					delete vec[i];
+				vec.erase(vec.begin() + 2, vec.end());
+				InvalidateRect(hWnd, NULL, false);
+				break;
+			}
+		case IDM_ADDPOINT:
+			DialogBox(hInst, (LPCTSTR)IDD_ADDPOINT, hWnd, (DLGPROC)addPointBox);
+			break;
+		case IDM_ADDLINE:
+			DialogBox(hInst, (LPCTSTR)IDD_ADDLINES, hWnd, (DLGPROC)addLineBox);
+			break;
+		case IDM_ADDPOLYGON:
+			DialogBox(hInst, (LPCTSTR)IDD_ADDPOLYGON, hWnd, (DLGPROC)addPolygonBox);
+			break;
+		case IDM_ADDCIRCLE:
+			DialogBox(hInst, (LPCTSTR)IDD_ADDCIRCLE, hWnd, (DLGPROC)addCircleBox);
+			break;
+		case IDM_SAVE: save(hWnd); break;
+		case IDM_HELP:
+			DialogBox(hInst, (LPCTSTR)IDD_HELPBOX, hWnd, (DLGPROC)Help);
+			break;
+		case IDM_DELETEPOINT:
+			vec[0]->eraseLast();
+			break;
+		case IDM_DELETELINE:
+			vec[1]->eraseLast();
+			break;
+		case IDM_DELETEOTHER:
+			if(vec.size() >2)
+				vec.erase(vec.end() -1);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	}
+
+	case WM_RBUTTONDOWN:
+		if(!isKeydown || g_mode != modePOLYGON) break;
+		addLastPoint(lParam);
+		break;
+	case WM_MOUSEMOVE:
+		if(!isKeydown) break;
+		switch(g_mode)
+		{
+		case modePENCIL: addLastPoint(lParam); break;
+		case modePOINTS: addPoints(lParam); break;
+		case modeLINES: modifyLastLine(lParam); break;
+		case modePOLYGON: 
+		case modeCIRCLE: modifyLastPoint(lParam); break;
+		}
+		InvalidateRect(hWnd,NULL,false);
+		break;
+	case WM_LBUTTONDOWN:
+		isKeydown = true;
+		switch(g_mode)
+		{
+		case modePENCIL:
+			{
+				Point pnt(LOWORD(lParam),g_clientRect.bottom - HIWORD(lParam));
+				PicElem* p = new Pencil;
+				p->add(pnt, g_defColor,g_defSize);
+				vec.push_back(p);
+			}
+			break;
+		case modePOINTS: addPoints(lParam); break;
+		case modeLINES:
+			addLines(lParam,lParam);
+			break;
+		case modePOLYGON:
+			{
+				Point pnt(LOWORD(lParam),g_clientRect.bottom - HIWORD(lParam));
+				PicElem* p = new Polygons;
+				p->add(pnt, pnt, g_defColor,g_defSize);
+				p->add(pnt, g_defColor,g_defSize);
+				vec.push_back(p);
+			}
+			break;
+		case modeCIRCLE:
+			{
+				Point pnt(LOWORD(lParam),g_clientRect.bottom - HIWORD(lParam));
+				PicElem* p = new Circles;
+				p->add(pnt, g_defColor, g_defSize);
+				vec.push_back(p);
+			}			
+		}
+		InvalidateRect(hWnd,NULL,false);
+		break;
+	case WM_LBUTTONUP:
+		isKeydown = false;
+		break;
+	case WM_SIZE:
+		{
+			GetClientRect(g_hwnd, &g_clientRect);
+			g_cliWidth = g_clientRect.right - g_clientRect.left;
+			g_cliHeight = g_clientRect.bottom - g_clientRect.top;
+			GetWindowRect(g_hwnd, &g_rect);
+			g_scrWidth = g_rect.right - g_rect.left;
+			g_scrHeight = g_rect.bottom - g_rect.top;
+			if(g_cliWidth > 0 && g_cliHeight > 0)
+				SceneResizeViewport(g_cliWidth, g_cliHeight);
+		}
+		break;
+	case WM_CHAR:
+		switch(wParam)
+		{
+		case '\n': case '\r':
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+			break;
+		case ' ':
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			break;				
+		}
+	case WM_PAINT:
+		SceneShow();
+		ValidateRect(hWnd,NULL);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+LRESULT CALLBACK Help(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+		
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) 
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		break;
+	}
+    return FALSE;
+}
+
+
+LRESULT CALLBACK inputBox(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+		
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) 
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		break;
+	}
+    return FALSE;
+}
+
+LRESULT CALLBACK colorBox(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+		
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDCANCEL) 
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		else if(LOWORD(wParam) == IDOK )
+		{
+			char r[128],g[128],b[128];
+			int ir,ig,ib;
+			ir= GetDlgItemText(hDlg, IDC_COLOR_R, r, 127);
+			ig = GetDlgItemText(hDlg, IDC_COLOR_G, g, 127);
+			ib = GetDlgItemText(hDlg, IDC_COLOR_B, b, 127);
+			r[ir] = 0;
+			g[ig] = 0;
+			b[ib] = 0;
+			g_defColor = Color(atof(r), atof(g), atof(b));
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		break;
+	}
+    return FALSE;
+}
+
+LRESULT CALLBACK sizeBox(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+		
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDCANCEL) 
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		else if(LOWORD(wParam) == IDOK)
+		{
+			char size[128];
+			size[GetDlgItemText(hDlg, IDC_DEFAULTWIDTH, size, 127)] = 0;
+			g_defSize = atof(size);
+			if(g_defSize == 0.0f) g_defSize = 1.0f;
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		break;
+	}
+    return FALSE;
+}
+
+
+LRESULT CALLBACK addPointBox(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+		
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDCANCEL) 
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		else if(LOWORD(wParam) == IDOK)
+		{
+			char x[128],y[128];
+			x[GetDlgItemText(hDlg, IDC_X, x, 127)] = 0;
+			y[GetDlgItemText(hDlg, IDC_Y, y, 127)] = 0;
+			vec[0]->add(Point(atof(x), g_cliHeight - atof(y)),g_defColor,g_defSize);
+			InvalidateRect(g_hwnd, NULL, false);
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		break;
+	}
+    return FALSE;
+}
+
+LRESULT CALLBACK addLineBox(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+		
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDCANCEL) 
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		else if(LOWORD(wParam) == IDOK)
+		{
+			char x1[128],y1[128],x2[128],y2[128];
+			x1[GetDlgItemText(hDlg, IDC_X1, x1, 127)] = 0;
+			y1[GetDlgItemText(hDlg, IDC_Y1, y1, 127)] = 0;
+			x2[GetDlgItemText(hDlg, IDC_X2, x2, 127)] = 0;
+			y2[GetDlgItemText(hDlg, IDC_Y2, y2, 127)] = 0;
+			vec[1]->add(Point(atof(x1), g_cliHeight - atof(y1)),Point(atof(x2), g_cliHeight - atof(y2)),g_defColor,g_defSize);
+			InvalidateRect(g_hwnd, NULL, false);
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		break;
+	}
+    return FALSE;
+}
+
+LRESULT CALLBACK addPolygonBox(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	static bool first = false;
+	static PicElem* p = NULL;
+	static int cnt = 0;
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		first = true;
+		p = new Polygons;
+		cnt = 0;
+		return TRUE;
+		
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDCANCEL) 
+		{
+			delete p;
+			p = NULL;
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		else if(LOWORD(wParam) == IDC_CONTINUE)
+		{
+			char x[128],y[128];
+			x[GetDlgItemText(hDlg, IDC_X, x, 127)] = 0;
+			y[GetDlgItemText(hDlg, IDC_Y, y, 127)] = 0;
+			if(x[0] && y[0])
+			{
+				Point pnt(atof(x),g_clientRect.bottom - atof(y));
+				p->add(pnt, g_defColor,g_defSize);
+				TextOut(GetDC(hDlg),10,90,x,sprintf(x,"第%d个点添加成功!",++cnt));
+			}
+			else
+				TextOut(GetDC(hDlg),10,90,x,sprintf(x,"输入内容不能为空!"));
+		}
+		else if(LOWORD(wParam) == IDOK)
+		{
+			char x[128],y[128];
+			x[GetDlgItemText(hDlg, IDC_X, x, 127)] = 0;
+			y[GetDlgItemText(hDlg, IDC_Y, y, 127)] = 0;
+			if(x[0] && y[0])
+			{
+				Point pnt(atof(x),g_clientRect.bottom - atof(y));
+				p->add(pnt, g_defColor,g_defSize);
+				vec.push_back(p);
+			}
+			else
+			{
+				TextOut(GetDC(hDlg),10,90,x,sprintf(x,"输入内容不能为空!"));
+				break;
+			}
+			InvalidateRect(g_hwnd, NULL, false);
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+	}
+    return FALSE;
+}
+
+LRESULT CALLBACK addCircleBox(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return TRUE;
+		
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) 
+		{
+			char x1[128],y1[128],x2[128],y2[128];
+			x1[GetDlgItemText(hDlg, IDC_X1, x1, 127)] = 0;
+			y1[GetDlgItemText(hDlg, IDC_Y1, y1, 127)] = 0;
+			x2[GetDlgItemText(hDlg, IDC_X2, x2, 127)] = 0;
+			y2[GetDlgItemText(hDlg, IDC_Y2, y2, 127)] = 0;
+			PicElem* p = new Circles;
+			p->add(Point(atof(x1),g_clientRect.bottom - atof(y1)), Point(atof(x2),g_clientRect.bottom - atof(y2)), g_defColor, g_defSize);
+			vec.push_back(p);
+			InvalidateRect(g_hwnd, NULL, false);
+			EndDialog(hDlg, LOWORD(wParam));
+			return TRUE;
+		}
+		break;
+	}
+    return FALSE;
+}
---
+++ src/resource.h
@@ -0,0 +1,85 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by OGLPaint.rc
+//
+#define IDC_MYICON                      2
+#define IDD_XY_DIALOG                   102
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDD_HELPBOX                     103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDS_HELLO                       106
+#define IDI_XY                          107
+#define IDI_SMALL                       108
+#define IDC_XY                          109
+#define IDR_MAINFRAME                   128
+#define IDD_INPUTPOINT                  130
+#define IDD_COLORBOX                    131
+#define IDD_DEFAULTWIDTH                132
+#define IDD_ADDPOINT                    133
+#define IDD_ADDLINES                    134
+#define IDD_ADDPOLYGON                  135
+#define IDD_ADDCIRCLE                   136
+#define IDC_RICHEDIT1                   1000
+#define IDC_RICHEDIT2                   1001
+#define IDC_RICHEDIT3                   1002
+#define IDC_RICHEDIT4                   1003
+#define IDC_COLOR_R                     1005
+#define IDC_COLOR_G                     1006
+#define IDC_COLOR_B                     1007
+#define IDC_DEFAULTWIDTH                1008
+#define IDC_RADIOPOINT                  1009
+#define IDC_RADIOLINES                  1010
+#define IDC_RADIOPOLYGON                1011
+#define IDC_RADIOCIRCLE                 1012
+#define IDC_ADDPOINTX                   1014
+#define IDC_INPUTPOINTY                 1015
+#define IDC_LINEX1                      1017
+#define IDC_LINEY1                      1018
+#define IDC_LINEX2                      1019
+#define IDC_LINEY2                      1020
+#define IDC_POLYGONX                    1022
+#define IDC_POLYGONCONTINUE             1023
+#define IDC_EDIT2                       1024
+#define IDC_Y                           1024
+#define IDC_Y1                          1024
+#define IDC_EDIT3                       1025
+#define IDC_X2                          1025
+#define IDC_EDIT4                       1026
+#define IDC_Y2                          1026
+#define IDC_EDIT5                       1027
+#define IDC_EDIT6                       1028
+#define IDC_X                           1031
+#define IDC_X1                          1032
+#define IDC_CONTINUE                    1033
+#define IDM_POINTS                      32771
+#define IDM_LINES                       32772
+#define IDM_Polygon                     32773
+#define IDM_CIRCLE                      32774
+#define IDM_SAVE                        32775
+#define IDM_COLOR                       32776
+#define IDM_SIZEBOX                     32777
+#define IDM_CLEAR                       32778
+#define IDM_PENCIL                      32779
+#define ID_MYWINDOW                     32780
+#define IDM_HELP                        32780
+#define IDM_ADDPOINT                    32782
+#define IDM_ADDLINE                     32783
+#define IDM_ADDPOLYGON                  32784
+#define IDM_ADDCIRCLE                   32785
+#define IDM_DELETEPOINT                 32786
+#define IDM_DELETELINE                  32787
+#define IDM_DELETEOTHER                 32788
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        138
+#define _APS_NEXT_COMMAND_VALUE         32789
+#define _APS_NEXT_CONTROL_VALUE         1037
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
